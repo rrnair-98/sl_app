13b05aed96af0ab9eb7639a781b89127497aed91
rrnair-98 <rrnair.98@gmail.com>
2017-09-30 13:38:55 +0530

    retrofit basic

---
+++ app/src/main/java/com/example/rohan/sl_app/networking/RetrofitApiInterface.java
@@ -0,0 +1,14 @@
+package com.example.rohan.sl_app.networking;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by rohan on 30/9/17.
+ */
+
+public interface RetrofitApiInterface {
+
+}
